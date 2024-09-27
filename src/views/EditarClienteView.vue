c2cb84d168b3a892013a41c5c1acebfb1d9b02ba
Edson Torres <66502687+EdsonTorres14@users.noreply.github.com>
2024-09-27 17:53:42 -0600

    218. Actualizando un cliente y diferencias entre PUT y PATCH

--- src/views/EditarClienteView.vue
+++ src/views/EditarClienteView.vue
@@ -42,7 +42,9 @@ defineProps({
 })
 
 const handleSumbit = (data) => {
-
+    ClienteService.actualizarCliente(id, data)
+        .then(() => router.push({ name: 'listado-clientes' }))
+        .catch(error => console.log(error))
 }
 </script>
 
@@ -57,7 +59,7 @@ const handleSumbit = (data) => {
 
         <div class="mx-auto mt-10 bg-white shadow">
             <div class="mx-auto md:w-2/3 py-20 px-6">
-                <FormKit type="form" submit-label="Agregar cliente"
+                <FormKit type="form" submit-label="Guardar cambios"
                     incomplete-message="No se pudo enviar, revisa los mensajes" @submit="handleSumbit"
                     :value="formData">
                     <FormKit type="text" label="Nombre" name="nombre" placeholder="Nombre del cliente"
